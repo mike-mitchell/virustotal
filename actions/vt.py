3744437b5e7067196268708c5013b9cfb4139d43
Mike Mitchell <mikemitchell@Mikes-MBP.verizon.net>
2017-02-06 13:47:49 -0500

    Initial Commit

---
+++ actions/vt.py
@@ -0,0 +1,13 @@
+import requests
+import json
+import urllib
+from st2actions.runners.pythonrunner import Action
+
+class VirusTotal(Action):
+    def run(self, apikey, IP):
+	url = 'https://www.virustotal.com/vtapi/v2/ip-address/report'
+	parameters = {'ip': IP, 'apikey': apikey}
+	response = urllib.urlopen('%s?%s' % (url, urllib.urlencode(parameters))).read()
+	response_dict = json.loads(response)
+	return (True, response_dict)
+
