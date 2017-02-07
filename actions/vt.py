55b71856a56a59ef509e2f08bbea475a66528155
Mike Mitchell <mikemitchell@Mikes-MBP.verizon.net>
2017-02-07 13:29:08 -0500

    fixed names

--- actions/vt.py
+++ actions/vt.py
@@ -4,9 +4,9 @@ import urllib
 from st2actions.runners.pythonrunner import Action
 
 class VirusTotal(Action):
-    def run(self, apikey, IP):
+    def run(self, apikey, ip):
 	url = 'https://www.virustotal.com/vtapi/v2/ip-address/report'
-	parameters = {'ip': IP, 'apikey': apikey}
+	parameters = {'ip': ip, 'apikey': apikey}
 	response = urllib.urlopen('%s?%s' % (url, urllib.urlencode(parameters))).read()
 	response_dict = json.loads(response)
 	return (True, response_dict)
